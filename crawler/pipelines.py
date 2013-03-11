66831cc211f9b69e860b02f319c07b138f6e7cfc
gongming <mg90@foxmail.com>
2013-03-11 16:29:36 +0800

    update datacore support

--- crawler/pipelines.py
+++ crawler/pipelines.py
@@ -7,7 +7,6 @@ import re
 from md5 import md5
 from crawler.db import mongo
 from datetime import datetime
-#from crawler.settings import 
 
 class RssPipeline(object):
     def process_item(self, item, spider):
@@ -37,22 +36,23 @@ class track_storage(object):
         if mongo.getdb().status.find({"statusid":item['statusid']}).count() is 0:
             mongo.getdb().status.insert(dict(item))
             mongo.getdb().user.update({"userid":item['statusuid']}, {"$inc":{'statistic.'+self.date:1}})
-            self.datacore(item)
+            self.datacore("WB"+item['statusuname'],item['content'])
 
     def process_rss_item(self, item):
         if mongo.getdb().rss.find({"md5":item['md5']}).count() is 0:
             if item['title'] is '' or item['content'] is '':
                 pass
             else:
-                re_l = re.compile("<!\[CDATA\[")
+                #re_l = re.compile("<!\[CDATA\[")
                 item['title'] = re.sub(ur'<[^>]*>','',re.sub(ur'\]\]>','',re.sub(ur'<!\[CDATA\[','',item['title'])))
                 item['content'] = re.sub(ur'<[^>]*>','',re.sub(ur'\]\]>','',re.sub(ur'<!\[CDATA\[','',item['content'])))
                 if item.get('date') is None:
                     item['date'] = self.date
                 mongo.getdb().rss.insert(dict(item))
+                self.datacore("RSS",item['content'])
 
-    def datacore(self, item):
-        #DATAAPI = {'app_key':476991604, 'app_secret':'315bd254d9d56da49e47261a278379cc'}
+    def datacore(self, username, content):
+        APIURL = "http://localhost/yqweibo/api.php"
         data = {}
         data['__API__[charset]'] = 'utf-8'
         data['__API__[output]'] = 'json' 
@@ -60,12 +60,10 @@ class track_storage(object):
         data['__API__[app_secret]'] = '610f11361c3f3b7e83a69b8ff3f9ebfd'#'315bd254d9d56da49e47261a278379cc' 
         data['__API__[username]'] = 'admin' 
         data['__API__[password]'] = md5('admin'+md5('admin').hexdigest()).hexdigest()
-        #  true    string      网站用户密码的加密字串，生成格式为：md5(用户名.md5(密码))
         data['mod'] = 'topic'
         data['code'] = 'add'
-        data['content'] = item['statusuname'] + ' ' + item['content']
+        data['content'] = username + '' + content.replace("#"," ").replace("@","&")
 
-        APIURL = "http://localhost/yqweibo/api.php"
         r = requests.post(APIURL,params=data)
         #print r.url
-        print r.json()['result']
+        #print r.json()['result']
